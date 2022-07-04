a18c9fe44b58ab390159b163701f5061a8a8ef23
widemar <widemarinternship@gmail.com>
2022-07-04 16:17:54 -0300

    C# Exercícios

---
+++ _04_Quadrante/Program.cs
@@ -0,0 +1,24 @@
+﻿bool coordenadaValida = true;
+
+while (coordenadaValida)
+{
+    Console.WriteLine("Digite os valores das coordenadas X e Y: ");
+    int x = int.Parse(Console.ReadLine()!);
+    int y = int.Parse(Console.ReadLine()!);
+
+    string mensagem = "";
+    if (x == 0 || y == 0)
+    {
+        coordenadaValida = false;
+    }
+    else
+        mensagem = x switch
+        {
+            > 0 when y > 0 => "Quadrante Q1",
+            < 0 when y > 0 => "Quadrante Q2",
+            < 0 when y < 0 => "Quadrante Q3",
+            _ => "Quadrante Q4"
+        };
+
+    Console.WriteLine(mensagem);
+}
---
+++ _05_ValidacaoDeNota/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+CultureInfo info = CultureInfo.InvariantCulture;
+
+Console.Write("Digite a primeira nota: ");
+double notaUm = double.Parse(Console.ReadLine()!, info);
+notaUm = VerificarIntervalo(notaUm);
+
+Console.Write("Digite a segunda nota: ");
+double notaDois = double.Parse(Console.ReadLine()!, info);
+notaDois = VerificarIntervalo(notaDois);
+
+double media = (notaUm + notaDois) / 2;
+Console.WriteLine($"MEDIA = {media.ToString("F2", info)}");
+
+
+double VerificarIntervalo(double nota)
+{
+    while (nota is < 0 or > 10)
+    {
+        Console.Write("Valor inválido! Tente novamente: ");
+        nota = double.Parse(Console.ReadLine()!, info);
+    }
+
+    return nota;
+}
