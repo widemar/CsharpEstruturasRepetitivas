a93a1a0fe976204fe249c969a2d5734c26ab0c4a
widemar <widemarinternship@gmail.com>
2022-07-04 14:32:09 -0300

    C# Exercícios

--- _01_Crescente/Program.cs
+++ _01_Crescente/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int numUm;
+int numDois;
+do
+{
+    Console.WriteLine("Digite dois números: ");
+    numUm = int.Parse(Console.ReadLine()!);
+    numDois = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Hello, World!");
+    if (numUm > numDois)
+    {
+        Console.WriteLine("DECRESCENTE!");
+    }
+    else if (numUm < numDois)
+    {
+        Console.WriteLine("CRESCENTE!");
+    }
+} while (numUm != numDois);
---
+++ _02_MediaIdades/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+CultureInfo info = CultureInfo.InvariantCulture;
+
+int idade;
+int somaIdades = 0;
+int qtdIdades = 0;
+Console.WriteLine("Digite as idades:");
+do
+{
+    idade = int.Parse(Console.ReadLine()!);
+    if (idade < 0 && qtdIdades == 0)
+    {
+        Console.WriteLine("IMPOSSÍVEL CALCULAR");
+    }
+    else if (idade > 0)
+    {
+        somaIdades += idade;
+        qtdIdades++;
+    }
+} while (idade > 0);
+
+double mediaIdades = (double)somaIdades / qtdIdades;
+if (qtdIdades > 0)
+{
+    Console.WriteLine($"MEDIA = {mediaIdades.ToString("F2", info)}"); 
+}
