df01c449f5c2a50d9b203c55a824166b13bbc0ea
widemar <widemarinternship@gmail.com>
2022-07-04 14:42:55 -0300

    C# Exercícios

---
+++ _03_SenhaFixa/Program.cs
@@ -0,0 +1,16 @@
+﻿bool senhaInvalida = true;
+
+while (senhaInvalida)
+{
+    Console.Write("Digite a senha: ");
+    int senha = int.Parse(Console.ReadLine()!);
+
+    while (senha != 2002)
+    {
+        Console.WriteLine("Senha Inválida! Tente novamente: ");
+        senha = int.Parse(Console.ReadLine()!);
+    }
+
+    Console.WriteLine("Acesso permitido!");
+    senhaInvalida = false;
+}
