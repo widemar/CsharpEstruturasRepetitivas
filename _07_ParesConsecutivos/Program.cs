918a4ae741616700c1372cd1d3a2d4097809103f
widemar <widemarinternship@gmail.com>
2022-07-04 22:15:38 -0300

    C# Exercícios

---
+++ _07_ParesConsecutivos/Program.cs
@@ -0,0 +1,29 @@
+﻿bool diferenteDeZero = true;
+
+
+while (diferenteDeZero)
+{
+    Console.Write("Digite um número inteiro: ");
+    int num = int.Parse(Console.ReadLine()!);
+
+    if (num == 0)
+    {
+        diferenteDeZero = false;
+    }
+    else
+    {
+        int qtdRepeticoes = 0;
+        int soma = 0;
+        while (qtdRepeticoes < 5)
+        {
+            if (num % 2 == 0)
+            {
+                soma += num;
+                qtdRepeticoes++;
+            }
+            num++;
+        }
+
+        Console.WriteLine($"SOMA {soma}");
+    }
+}
