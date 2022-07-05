7c06aad70fd755c6c1dd1aa84730b9c465c09e08
widemar <widemarinternship@gmail.com>
2022-07-05 09:55:07 -0300

    C# Exercícios

---
+++ _16_Experiencias/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+CultureInfo info = CultureInfo.InvariantCulture;
+
+Console.Write("Quantos casos de teste serão digitados: ");
+int qtdDeTestes = int.Parse(Console.ReadLine()!);
+
+int contaCoelhos = 0, contaRatos = 0, contaSapos = 0;
+for (int i = 0; i < qtdDeTestes; i++)
+{
+    Console.Write("Quantidade de cobaias: ");
+    int qtdCobaias = int.Parse(Console.ReadLine()!);
+    Console.Write("Tipo de cobaia: ");
+    char tipoDeCobaia = char.Parse(Console.ReadLine()!.ToUpper());
+
+    switch (tipoDeCobaia)
+    {
+        case 'C':
+            contaCoelhos += qtdCobaias;
+            break;
+        case 'R':
+            contaRatos += qtdCobaias;
+            break;
+        case 'S':
+            contaSapos += qtdCobaias;
+            break;
+        default:
+            Console.WriteLine("Cobaia Inválida");
+            break;
+    }
+}
+
+int totalCobaias = contaCoelhos + contaRatos + contaSapos;
+double percentualDeCoelhos = contaCoelhos / (double)totalCobaias * 100;
+double percentualDeRatos = contaRatos / (double)totalCobaias * 100;
+double percentualDeSapos = contaSapos / (double)totalCobaias * 100;
+
+Console.WriteLine($"Total: {totalCobaias}");
+Console.WriteLine($"Total de coelhos: {contaCoelhos}");
+Console.WriteLine($"Total de ratos: {contaRatos}");
+Console.WriteLine($"Total de sapos: {contaSapos}");
+Console.WriteLine($"Percentual de coelhos: {percentualDeCoelhos.ToString("F2", info)}");
+Console.WriteLine($"Percentual de ratos: {percentualDeRatos.ToString("F2", info)}");
+Console.WriteLine($"Percentual de sapos: {percentualDeSapos.ToString("F2", info)}");
---
+++ _17_DesafioAtletas/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Globalization;
+
+CultureInfo info = CultureInfo.InvariantCulture;
+
+Console.Write("Qual a quantidade de atletas? ");
+int qtdDeAtletas = int.Parse(Console.ReadLine()!);
+
+double somaPesos = 0;
+string nomeAtletaMaisAlto = "";
+double alturaAtletaMaisAlto = 0;
+int contaHomens = 0, contaMulheres = 0;
+double somaAlturaMulheres = 0;
+for (int i = 1; i <= qtdDeAtletas; i++)
+{
+    Console.WriteLine($"Digite os dados do atleta {i}:");
+    Console.Write("Nome: ");
+    string nome = Console.ReadLine()!;
+
+    Console.Write("Sexo: ");
+    char sexo = char.Parse(Console.ReadLine()!.ToUpper());
+    while (sexo != 'M' && sexo != 'F')
+    {
+        Console.Write("Valor inválido! Favor digitar F ou M: ");
+        sexo = char.Parse(Console.ReadLine()!.ToUpper());
+    }
+
+    Console.Write("Altura: ");
+    double altura = double.Parse(Console.ReadLine()!, info);
+    while (altura <= 0)
+    {
+        Console.Write("Valor inválido! Favor digitar um valor positivo: ");
+        altura = double.Parse(Console.ReadLine()!, info);
+    }
+
+    if (altura > alturaAtletaMaisAlto)
+    {
+        nomeAtletaMaisAlto = nome;
+        alturaAtletaMaisAlto = altura;
+    }
+
+    if (sexo == 'M')
+    {
+        contaHomens++;
+    }
+    else
+    {
+        contaMulheres++;
+        somaAlturaMulheres += altura;
+    }
+
+    Console.Write("Peso: ");
+    double peso = double.Parse(Console.ReadLine()!, info);
+    while (peso <= 0)
+    {
+        Console.Write("Valor inválido! Favor digitar um valor positivo: ");
+        peso = double.Parse(Console.ReadLine()!, info);
+    }
+
+    somaPesos += peso;
+}
+
+double pesoMedio = somaPesos / qtdDeAtletas;
+double porcentagemDeHomens = (double)contaHomens / qtdDeAtletas * 100;
+
+Console.WriteLine("RELATÓRIO:");
+Console.WriteLine($"Peso médio dos atletas: {pesoMedio.ToString("F2", info)}");
+Console.WriteLine($"Atleta mais alto: {nomeAtletaMaisAlto}");
+Console.WriteLine($"Porcentagem de homens: {porcentagemDeHomens.ToString("F1", info)}%");
+if (contaMulheres == 0)
+{
+    Console.WriteLine("Não há mulheres cadastradas.");
+}
+else
+{
+    double alturaMediaMulheres = somaAlturaMulheres / contaMulheres;
+    Console.WriteLine($"Altura média das mulheres: {alturaMediaMulheres.ToString("F2", info)}");
+}
